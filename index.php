f4a5facde9ffab594447851ca5ebc78cbb8ecaad
Ney Filho <neyfilho92@gmail.com>
2021-04-23 15:37:23 -0300

    feat: ajustes e inclusoes de funcionalidade geral

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-set_time_limit(300);
+ini_set('MAX_EXECUTION_TIME', '-1');
+set_time_limit(30000000);
 
 include_once "connect.php";
 include_once "importcsv.php";
@@ -17,91 +18,112 @@ if (isset($_POST["Import"])) {
     importCsv($file, $tablename, $delimiter);
 }
 
+if (isset($_POST["Update"])) {
+
+    $tablename = $_POST["tablename"];
+    $fieldTable = $_POST["fieldTable"];
+    $fieldUpdate = $_POST["fieldUpdate"];
+
+    updateField($tablename, $fieldTable, $fieldUpdate);
+}
+
 ?>
 
 <html>
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" crossorigin="anonymous">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <title>Index</title>
     </head>
     <body>
-        <div class="container pt-3">
-            <div class="row">
+
+        <!-- Import csv -->
+        <div class="container-fluid">
+            <div class="row justify-content-center">
                 <form class="form-horizontal" method="post" action="index.php" enctype="multipart/form-data">
-                    <fieldset>
-                        
-                        <!-- Form Name -->
-                        <legend>Importação e Exportação de CSV</legend>
                         
                         <!-- Select Tables Box -->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="exampleFormControlSelect1">Selecione a base de dados:</label>
-                            <div class="col-md-4">
+                            <label class="control-label" for="exampleFormControlSelect1">Selecione a base de dados:</label>
+                            <div>
                                 <select name="tablename"  class="form-control" id="exampleFormControlSelect1">
-                                    <option value="tableone">tableone</option>
-                                    <option value="tabletwo">tabletwo</option>
+                                    <option value="tableone">Tableone</option>
+                                    <option value="tabletwo">Tabletwo</option>
                                 </select>
                             </div>
                         </div>
 
                         <!-- Select Delimiter Box -->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="exampleFormControlSelect1">Selecione o delimitador:</label>
-                            <div class="col-md-4">
+                            <label class="control-label" for="exampleFormControlSelect1">Selecione o delimitador:</label>
+                            <div>
                                 <select name="delimitador"  class="form-control" id="exampleFormControlSelect1">
                                     <option value="<?php echo "," ?>"">Virgula</option>
                                     <option value="<?php echo "\t" ?>">Tab</option>
+                                    <option value="<?php echo ";" ?>">Ponto e Virgula</option>
                                 </select>
                             </div>
                         </div>
 
                         <!-- File Button -->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="exampleFormControlFile1">Envie seu csv:</label>
-                            <div class="col-md-4">
+                            <label class="control-label" for="exampleFormControlFile1">Envie seu csv:</label>
+                            <div>
                                 <input type="file" name="file"/>
                             </div>
                         </div>
                         
                         <!-- Button -->
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="singlebutton">Import data</label>
-                            <div class="col-md-4">
+                            <div style="text-align: center;">
                                 <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Upload</button>
                             </div>
                         </div>
 
-                    </fieldset>
                 </form>
             </div>
         </div>
+
+        <!-- Import Tables -->
         <div class="container">
-            <div class="row">
-                <h3>Tabela 1</h3>
+            <div class="row justify-content-center">
                 <?php getAll("tableone"); ?>
-            </div>
-            <div class="row">
-                <p>Foram encontrados <?php echo countAll("tableone");?>
-                 registros.</p>
-            </div>
-            <div class="row">
-                <h3>Tabela 2</h3>
                 <?php getAll("tabletwo"); ?>
-            </div>
-            <div class="row">
-                <p>Foram encontrados <?php echo countAll("tabletwo");?>
-                 registros.</p>
-            </div>
-            <div class="row">
-                <h3>Diff de Tabelas</h3>
-                <?php getAll("tabletwo"); ?>
-            </div>
-            <div class="row">
-                <p>Foram encontrados <?php echo countAll("tabletwo");?>
-                 registros.</p>
             </div>
         </div>
+
+        <!-- Export csv -->
+        <div class="container">
+            <div class="row justify-content-center">
+                <?php if(getAllDiff() != null) ?>
+            </div>
+        </div>
+
+        <!-- Form update fields -->
+        <div class="container">
+            <div class="row justify-content-center">
+                <form class="form-horizontal" method="post" action="index.php">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Selecione a tabela a ser atualizada:</label>
+                        <select name="tablename"  class="form-control" id="exampleFormControlSelect1">
+                            <option value="tableone">Tableone</option>
+                            <option value="tabletwo">Tabletwo</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Campo a ser atualziado:</label>
+                        <input type="text" class="form-control" name="fieldTable">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Novo nome para atualização:</label>
+                        <input type="text" class="form-control" name="fieldUpdate">
+                    </div>
+                    <button name="Update" type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+
     </body>
 </html>
--- list.php
+++ list.php
@@ -8,11 +8,12 @@ function getAll($tablename)
 
     if ($data != null) {
         echo "<div class='table-responsive col-md-4'>
-                <table style='text-align: center' id='myTable' class='table table-striped table-bordered'>
+                <h3 style='text-align: center;'>Tabela: ".$tablename."</h3>
+                <table style='text-align: center;' id='myTable' class='table table-striped table-bordered'>
                     <thead>
                         <tr>
-                            <th style='text-align: center'>Id</th>
-                            <th style='text-align: center'>Name</th>
+                            <th style='text-align: center;'>Id</th>
+                            <th style='text-align: center;'>Name</th>
                         </tr>
                     </thead>
                 <tbody>";
@@ -23,16 +24,76 @@ function getAll($tablename)
                 echo "<td>" . $row['name']. "</td>";
                 echo "<tr>";
             }
-        echo "</tbody></table></div>";
+        echo "</tbody></table><p style='text-align: center;'>Registros encontrados: ".countAll($tablename)."</p></div>";
         
    } else {
         echo "Você ainda não possui registros na tabela.";
    }
 }
 
+function getAllDiff()
+{
+    $data = connectDb()->query("SELECT * FROM tabletwo INNER JOIN tableone ON tabletwo.id = tableone.id  WHERE tableone.name <> tabletwo.name limit 5")->fetchAll();
+
+    if ($data != null) {
+        echo "<div class='table-responsive col-md-4'>
+                <h3 style='text-align: center;'>Tabela Diff</h3>
+                <table style='text-align: center;' id='myTable' class='table table-striped table-bordered'>
+                    <thead>
+                        <tr>
+                            <th style='text-align: center;'>Id</th>
+                            <th style='text-align: center;'>Name</th>
+                        </tr>
+                    </thead>
+                <tbody>";
+   
+            foreach ($data as $row) {
+                echo "<tr>";
+                echo "<th scope='row'>" . $row['id']. "</th>";
+                echo "<td>" . $row['name']. "</td>";
+                echo "<tr>";
+            }
+        echo "</tbody></table><p style='text-align: center;'>Registros diferentes encontrados: ".countDiff()."</p>";
+        echo "<div>
+                <form class='form-horizontal' action='index.php' method='post' name='upload_excel' enctype='multipart/form-data'>
+                    <div class='form-group'>
+                        <div style='text-align: center;'>
+                            <input type='submit' name='Export' class='btn btn-success' value='export to excel'/>
+                        </div>
+                    </div>                    
+                </form>           
+            </div></div>";
+        
+   }
+}
+
+function updateField($tablename, $fieldTable, $fieldUpdate)
+{
+    $stmt = connectDb()->prepare("UPDATE $tablename SET name = '$fieldUpdate' WHERE name = '$fieldTable'");
+    $result = $stmt->execute();
+
+    if(!$result ){
+        echo "<script type=\"text/javascript\">
+                alert(\"Não foi possivel atualizar o campo.\");
+                window.location = \"index.php\"
+            </script>";		
+    }
+
+    echo "<script type=\"text/javascript\">
+            alert(\"Campo atualizado com sucesso.\");
+            window.location = \"/\"
+        </script>";
+}
+
 function countAll($tablename)
 {
     $data = connectDb()->query("SELECT * FROM $tablename")->rowCount();
+    return $data;
+}
+
+function countDiff()
+{
+    $data = connectDb()->query("SELECT * FROM tabletwo INNER JOIN tableone ON tabletwo.id = tableone.id  WHERE tableone.name <> tabletwo.name")->rowCount();
     return $data;
 }
 
